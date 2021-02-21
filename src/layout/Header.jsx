8cd34d25db3a49064144c84d973cb658eff17bca
Karen <karenchakhalyan23@gmail.com>
2021-02-22 00:34:03 +0400

    Add React App

---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header () {
+    return <nav>
+        <div className="nav-wrapper">
+        <a href="#" className="brand-logo">React movies</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="!#">Repository</a></li>
+        </ul>
+        </div>
+</nav>
+}
+
+export {Header}
