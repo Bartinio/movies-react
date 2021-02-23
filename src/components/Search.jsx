b0c0043e103d1a90c0e9cd8dc925992fc22aa0b3
Karen <karenchakhalyan23@gmail.com>
2021-02-23 23:11:27 +0400

    added serch function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
 class Search extends React.Component{
+
+    state = {
+        search: '',
+
+    }
+
+    handleKey = (event) => {
+        if (event.key === 'Enter') {
+            this.props.searchMovies(this.state.search)
+        }
+    } 
     render() {
         return <div className="row">
         <div className="input-field col s12">
           <i className="material-icons prefix" style={{color: '#ee6e73' }}>search</i>
-          <input id="icon_prefix" type="search" className="validate"  placeholder="Search"/>
+          <input 
+            className="validate" 
+            id="icon_prefix"
+            type="search" 
+            placeholder="Search"
+            value={this.state.search}
+            onChange={(e) => this.setState({search: e.target.value})}
+            onKeyDown={this.handleKey}
+         />
         </div>
         </div>
     }
